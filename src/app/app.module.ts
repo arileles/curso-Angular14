ea907403647e6ab0e842efa1a7aba068c1b38094
Ariel Louise <Ariel.Louise@senior.com.br>
2025-03-13 10:27:27 -0300

    enviando projeto angular 14

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule, Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { ParagrafoComponent } from './componentes/paragrafo/paragrafo.component';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CabecalhoComponent,
+    RodapeComponent,
+    ParagrafoComponent,
+    CriarPensamentoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/componentes/paragrafo/paragrafo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-paragrafo',
+  templateUrl: './paragrafo.component.html',
+  styleUrls: ['./paragrafo.component.css']
+})
+export class ParagrafoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
